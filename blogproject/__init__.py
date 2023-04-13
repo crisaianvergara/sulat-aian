224e13406d7116641240f4508ca23105e51124cd
Cris-Aian <crisaianvergara@gmail.com>
2023-04-14 02:20:48 +0800

    deploying on render

--- blogproject/__init__.py
+++ blogproject/__init__.py
@@ -4,11 +4,20 @@ import os
 
 # Flask App
 app = Flask(__name__)
+
 app.config["SECRET_KEY"] = os.environ.get("FLASK_SECRET_KEY")
+
+# postgres://sulat_aian_user:BaaCICVYLsHJNrhi95F5Yxe3FpXoiREP@dpg-cgs46d02qv20m9nfebug-a.oregon-postgres.render.com/sulat_aian
 
 # Database
 app.app_context().push()
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///blog.db"
+
+# Local
+# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///blog.db"
+
+# Render
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL_SULAT")
+
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
